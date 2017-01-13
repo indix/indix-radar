576fab9ffcf320a3a854a8308f65ef1b1e54d731
GP <ganeshp@indix.com>
2017-01-13 11:43:39 +0530

    🚀 Deploy script for Indix-Radar, inspired by https://github.com/indix/indix.github.io/blob/source/deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Exit immediately if something exits with error(s).
+set -e
+
+# Install deps.
+npm install hexo-cli@1.0.2 webpack@1.14.0 --global
+npm install
+
+# Generate Hexo static site.
+hexo clean
+hexo generate
+webpack
+
+# BAM! Deploy.
+hexo deploy
