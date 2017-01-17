def7a1cba60c6e66c4e84237aad9c1fa9ed4604d
GP <ganeshp@indix.com>
2017-01-17 17:06:07 +0530

    [BUILD] Try to give permissions for the bash file that Snap-CI seems to use during Yarn installation.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,12 @@
 set -e
 
 # Install deps.
+BASH_FILE="/var/go/.bashrc"
+if [ -e "$BASH_FILE" ]; then
+    if [ ! -w "$BASH_FILE" ]; then
+        sudo chmod u+w "$BASH_FILE" && echo "$BASH_FILE is now writable!"
+    fi
+fi
 sudo curl -o- -L https://yarnpkg.com/install.sh | bash
 npm install hexo-cli@1.0.2 webpack@1.14.0 --global
 yarn install
