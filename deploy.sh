5854c29c341d16674f9c8faaf9428a6c243f5b5d
GP <ganeshp@indix.com>
2017-01-17 17:26:49 +0530

    [BUILD] 🙌🏽

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,8 @@
 # Exit immediately if something exits with error(s).
 set -e
 
-# Snap-CI specific stuff.
-BASH_FILE="/var/go/.bashrc"
-if [ -e "$BASH_FILE" ]; then
-    if [ ! -w "$BASH_FILE" ]; then
-        sudo chmod u+w "$BASH_FILE"
-        echo "$BASH_FILE is now writable!"
-        source "$BASH_FILE"
-    fi
-fi
-# End Snap-CI specific stuff.
-
 # Install Yarn.
-sudo curl -o- -L https://yarnpkg.com/install.sh | bash
-export PATH="$HOME/.yarn/bin:$PATH"
+npm install --global yarn
 
 # Install Hexo & WebPack.
 npm install hexo-cli@1.0.2 webpack@1.14.0 --global
@@ -27,7 +15,7 @@ yarn install --production
 # Generate Hexo static site.
 hexo clean
 hexo generate
-webpack
+./node_modules/webpack/bin/webpack.js
 
 # BAM! Deploy.
 hexo deploy
