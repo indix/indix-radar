5b81c224c2eb141415aaf73570841cb6e6086d7f
GP <ganeshp@indix.com>
2017-01-17 17:08:47 +0530

    [BUILD] Yarn can just write a better installation script.

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,19 @@
 # Exit immediately if something exits with error(s).
 set -e
 
-# Install deps.
+# Install global deps.
+
+# Snap-CI specific stuff.
 BASH_FILE="/var/go/.bashrc"
 if [ -e "$BASH_FILE" ]; then
     if [ ! -w "$BASH_FILE" ]; then
-        sudo chmod u+w "$BASH_FILE" && echo "$BASH_FILE is now writable!"
+        sudo chmod u+w "$BASH_FILE"
+        echo "$BASH_FILE is now writable!"
+        source "$BASH_FILE"
     fi
 fi
+# End Snap-CI specific stuff.
+
 sudo curl -o- -L https://yarnpkg.com/install.sh | bash
 npm install hexo-cli@1.0.2 webpack@1.14.0 --global
 yarn install
