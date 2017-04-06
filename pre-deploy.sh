ba8d0dc8662d39d63c3053c7da1af1637955d4e5
GP <ganeshp@indix.com>
2017-04-06 15:35:15 +0530

    Add a new pre-deploy script that can also be used as a Git pre-commit hook that validates YML.

---
+++ pre-deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Test for YML errors in data files.
+# You can also use this as a pre-commit hook by using the command -
+#
+# cp pre-deploy.sh ./git/hooks/pre-commit
+
+EMOJI="\xE2\x9A\xA1"
+
+echo "‍‍$EMOJI Testing for YML errors..."
+for file in source/_data/*.yml
+do
+    ./node_modules/.bin/js-yaml "$file" > /dev/null
+	rc=$?
+	if [[ $rc != 0 ]]
+	then
+		echo "$EMOJI Aborting commit due to YML validation failure!"
+		exit $rc
+	fi
+done
+echo "‍‍$EMOJI No YML errors found!"
+exit 0
